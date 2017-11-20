c24aa435e7955213cade9373219123ada9d879e0
ninon lafourcade <ninon@nout.fr>
2017-11-20 16:06:15 +0100

    réorganisation des exemples

--- PHP/index.php
+++ PHP/index.php
@@ -1,12 +1,21 @@
 <html>
 <head>
-<title>SOAPClient Exemple code</title>
+    <title>SOAPClient Exemple code</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous" />
 </head>
 <body>
-<ul>
-    <li><a href="connexion_only.php">Connexion / Déconnexion</a></li>
-    <li><a href="connexionextranet_only.php">Connexion Extranet / Déconnexion</a></li>
-    <li><a href="creer_ville_pays.php">Créer ville</a></li>
-</ul>
+<div class="container">
+
+    <h1>Sur Homer / simaxV12</h1>
+    <div class="list-group">
+        <a class="list-group-item list-group-item-action" href="SIMAXV12_connexion_only.php">Connexion / Déconnexion</a>
+        <a class="list-group-item list-group-item-action" href="SIMAXV12_connexionextranet_only.php">Connexion Extranet / Déconnexion</a>
+        <a class="list-group-item list-group-item-action" href="SIMAXV12_creer_ville_pays.php">Créer ville</a></li>
+    </div>
+    <h1>Sur Prod / Validator</h1>
+    <div class="list-group">
+        <a class="list-group-item list-group-item-action" href="VALIDATOR_rechercher_modifier_element.php">Cocher vrai/faux type de champ simple</a>
+    </div>
+</div>
 </body>
 </html>
